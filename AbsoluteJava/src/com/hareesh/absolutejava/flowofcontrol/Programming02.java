25009b911d3226eb892aa2f972728bebec6cfe49
hareeshkavumkulath <43506791+hareeshkavumkulath@users.noreply.github.com>
2018-12-17 21:04:46 -0500

    Programming02 - first commit

---
+++ AbsoluteJava/src/com/hareesh/absolutejava/flowofcontrol/Programming02.java
@@ -0,0 +1,15 @@
+package com.hareesh.absolutejava.flowofcontrol;
+
+import java.util.Scanner;
+
+public class Programming02 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String player1 = in.nextLine();
+		String player2 = in.nextLine();
+		System.out.println("Player1: " + player1);
+		System.out.println("Player2: " + player2);
+	}
+
+}
